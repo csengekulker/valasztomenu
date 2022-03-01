fc692e26c7a73a1fa51bb68c27490503bf86be0e
csengekulker <k71608883429@ktch.hu>
2022-03-01 09:14:59 +0100

    why delete bin/.gitkeep :(

--- src/App.java
+++ src/App.java
@@ -4,33 +4,40 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("---------------------");
-        System.out.println();
-        System.out.println("(1) Program inditasa");
-        System.out.println();
-        System.out.println("(2) Nevjegy");
-        System.out.println();
-        System.out.println("(3) Kilepes");
-        System.out.println();
+        String choice;
 
-        System.out.print("Valasztas > ");
-        String choice = sc.nextLine();
 
-        // switchcase
 
-        switch (choice) {
-            case "1":
-                System.out.println("egyes");
-                break;
-            case "2":
-                System.out.println("kettes");
-                break;
-            case "3":
-                System.out.println("harmas");
-                break;
-            default:
-                break;
-        }
+        // switchcase (do whileba)
+
+        do {
+            System.out.println("---------------------");
+            System.out.println();
+            System.out.println("(1) Program inditasa");
+            System.out.println();
+            System.out.println("(2) Nevjegy");
+            System.out.println();
+            System.out.println("(3) Kilepes");
+            System.out.println();
+    
+            System.out.print("Valasztas > ");
+            choice = sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("egyes");
+                    break;
+                case "2":
+                    System.out.println("kettes");
+                    break;
+                case "3":
+                    System.out.println("harmas");
+                    break;
+            }
+
+        } while ( !choice.equals("3") );
+
+
 
         // modositok
 
