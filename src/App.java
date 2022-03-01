582e6b2e69f84b5cfdc69fdc52116bd151bb7e9f
csengekulker <k71608883429@ktch.hu>
2022-03-01 08:59:37 +0100

    program prints options, asks choice

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---------------------");
+        System.out.println();
+        System.out.println("(1) Program inditasa");
+        System.out.println();
+        System.out.println("(2) Nevjegy");
+        System.out.println();
+        System.out.println("(3) Kilepes");
+        System.out.println();
+
+        System.out.print("Valasztas > ");
+        String choice = sc.nextLine();
+
+        sc.close();
     }
 }
